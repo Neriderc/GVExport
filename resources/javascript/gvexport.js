bd4f199793ebec5330f2d63b9e92dcfc6d3de88c
Neriderc <89974636+Neriderc@users.noreply.github.com>
2022-08-28 09:54:29 +1200

    Add error when canvas too big
    If exporting file uses canvas (e.g. JPEG, PNG) then the browser has a max canvas size that varies by browser and in most cases will result in a blank canvas and an invalid zero kilobyte file downloaded. If the output is very short we have hit this limit, so we show an error message.

--- resources/javascript/gvexport.js
+++ resources/javascript/gvexport.js
@@ -242,7 +242,11 @@ function downloadSVGAsImage(type) {
         context.drawImage(img, 0, 0, img.width, img.height);
         // Download it
         const dataURL = canvas.toDataURL('image/'+type);
-        downloadLink(dataURL, "gvexport."+type);
+        if (dataURL.length < 10) {
+            showToast("E:"+CLIENT_ERRORS[0]); // Canvas too big
+        } else {
+            downloadLink(dataURL, "gvexport." + type);
+        }
     }
 
 }
