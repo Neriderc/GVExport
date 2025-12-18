39bce051a71a9107fef7837a0b933af2509f87ec
Neriderc <89974636+Neriderc@users.noreply.github.com>
2025-12-18 13:44:24 +1300

    Minor tweaks

--- app/FormSubmission.php
+++ app/FormSubmission.php
@@ -503,7 +503,7 @@ class FormSubmission
      */
     private function isPrefixStringValid($name): bool
     {
-        return preg_match('/^[A-Za-z0-9_ .*+()^∞%$#@!†-↑↓×⚮|]*$/',$name);
+        return preg_match('/^[A-Za-z0-9_ .*+()^∞%$#@!†-↑↓×⚮\\|]*$/',$name);
     }
 
     /**
