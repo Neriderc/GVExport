73c9b0b2e9858ce55747978e4f8568b5c4bc9cbc
Neriderc <89974636+Neriderc@users.noreply.github.com>
2026-01-02 20:43:01 +1300

    Add tests for pedigree arrow labels

--- playwright/tests/guest/guestOptions.spec.ts
+++ playwright/tests/guest/guestOptions.spec.ts
@@ -1,7 +1,21 @@
 import { runSharedOptionsTests } from '../common/sharedOptionTests.ts'
 import { test, expect } from '../../fixtures.ts';
+import { loadGVExport } from '../common/utils.ts';
 
 /**
  * Run our suite of tests that are common across user and guest
  */
 runSharedOptionsTests('user');
+
+test('option: Add arrow label when pedigree type is not "birth"', async ({ page }) => {
+        await loadGVExport(page, true);
+        await page.locator('#show_pedigree_type').check();
+        await page.waitForSelector('svg');
+        const svgHtml = await page.locator('#rendering svg').innerHTML();
+
+        // Values not expected as private when logged out
+        await expect(svgHtml).not.toContain('Radāʿ');
+        await expect(svgHtml).not.toContain('Sealing');
+        await expect(svgHtml).not.toContain('Foster');
+        await expect(svgHtml).not.toContain('Adopted by both parents');
+    });
