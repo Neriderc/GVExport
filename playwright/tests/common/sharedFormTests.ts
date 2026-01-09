9bd3f0d1abaf56a3e56020e3804a2ea67b249638
Neriderc <89974636+Neriderc@users.noreply.github.com>
2026-01-10 12:37:24 +1300

    Remove duplicated call
    This is not needed as loadGVExport now does this. Duplication was causing test to fail

--- playwright/tests/common/sharedFormTests.ts
+++ playwright/tests/common/sharedFormTests.ts
@@ -12,8 +12,6 @@ export function runSharedFormTests(role: 'guest' | 'user') {
 
         test('non-default form values save', async ({ page }) => {
             await loadGVExport(page);
-            await page.locator('li.menu-language > a.dropdown-toggle').click();
-            await page.getByRole('menuitem', { name: 'British English' }).click();
             await setNonDefaults(page, false);
             await page.goto('/module/_GVExport_/Chart/gvetest?xref=X1'); 
             await checkNonDefaults(page, false);
